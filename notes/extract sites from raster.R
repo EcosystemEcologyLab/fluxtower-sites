e41de0e47e8013db6cf339e92d2256d6d1c5021d
Eric Scott <scottericr@gmail.com>
2024-06-27 15:51:01 -0700

    comment code

--- notes/extract sites from raster.R
+++ notes/extract sites from raster.R
@@ -17,6 +17,7 @@ raster <- rast(file_path)
 sites_df <- 
   read_csv("fluxtower_locations.csv") 
 
+#conver to sf object
 sites_sf <- 
   sites_df |> 
   st_as_sf(coords = c("lon", "lat")) |> 
@@ -25,18 +26,23 @@ sites_sf <-
 
 
 #get raster boundaries
-raster_bbox <- st_bbox(raster) |> st_as_sfc() |> st_set_crs(crs(raster))
+raster_bbox <- st_bbox(raster) |>
+  st_as_sfc() |> #needs to be sfc object
+  st_set_crs(crs(raster)) #needs to have CRS set
 
-#filter sites to only those within raster boundaries
 sites_buffer <-
   sites_sf |> 
+  #filter sites to only those within raster boundaries
   st_intersection(raster_bbox) %>% 
+  #create circle polygons with buffer in meters
   st_buffer(dist = set_units(.$buffer_radius_m, "m")) |> 
   #add column with folder name on snow
   mutate(product = path_file(path_dir(file_path)))
 
 # extract total AGB from each site
 df <- exact_extract(raster, sites_buffer, fun = "sum", append_cols = TRUE, force_df = TRUE)
+
+# tidy data
 df |> 
   pivot_longer(
     starts_with("sum."),
