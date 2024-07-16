9303282c823e074494454b879c22b0485c5cbc14
Eric Scott <ericrscott@arizona.edu>
2024-07-16 11:32:30 -0700

    update notes to work with new data using footprint calculation

---
+++ R/calc_ffp_radius.R
@@ -0,0 +1,23 @@
+#a simple function for estimating reasonable radius for the footprint of a flux tower
+#from Kljun et al. 2015, equn 25, pg 3702
+
+#zm is the sensor height above the displacement height (zm = meas_h - d); d has been assumed 2/3*canopy height
+#friction velocity = ustar in flux data
+
+calc_ffp_radius <- function(zm, wind_speed, friction_velocity){
+  
+  #define tower constants
+  footprint_fraction = 0.85 #valid for values 0.1 to 0.9
+  Kljun_param_c = 1.462 #see paper
+  Kljun_param_d = 0.136 #see paper
+  boundary_layer_height = 1000 #boundary layer assumed stable, so height = 1000m
+  von_Karman_constant = 0.4
+  
+  #calculate tower radius
+  ffp_radius = 
+    (((-Kljun_param_c)/(log(footprint_fraction))) + Kljun_param_d) *
+    zm * ((1 - (zm / boundary_layer_height))^(-1)) *
+    ((wind_speed * zm) / friction_velocity) * von_Karman_constant
+  
+  return(ffp_radius)
+}
--- notes/extract sites from raster.R
+++ notes/extract sites from raster.R
@@ -1,3 +1,5 @@
+# R code for extracting zonal statistics for all fluxtower sites from a single AGB product
+
 library(fs)
 library(sf)
 library(terra)
@@ -6,6 +8,8 @@ library(exactextractr)
 library(units)
 library(tidyr)
 library(dplyr)
+sf_use_s2(TRUE)
+source("R/calc_ffp_radius.R")
 
 # root <- "/Volumes/moore/AGB_cleaned"
 root <- "d://AGB_cleaned/"
@@ -18,14 +22,22 @@ raster <- rast(file_path)
 
 #read in sites csv
 sites_df <- 
-  read_csv("fluxtower_locations.csv") 
+  read_csv("data/ffp_inputs.csv") 
 
 #convert to sf object
 sites_sf <- 
   sites_df |> 
-  st_as_sf(coords = c("lon", "lat")) |> 
+  st_as_sf(coords = c("LOCATION_LONG", "LOCATION_LAT")) |> 
   st_set_crs("WGS84") |> 
   st_transform(crs(raster))
+
+#calculate flux tower footprint
+sites_sf <- 
+  sites_sf |> 
+  mutate(radius = calc_ffp_radius(zm = ZM_F, wind_speed = avg_WS, friction_velocity = avg_ustar)) |> 
+  #remove sites with unknown footprint
+  #TODO an alternative here would be to pick a default radius if one can't be calculated
+  filter(!is.na(radius))
 
 
 #get raster boundaries
@@ -34,22 +46,35 @@ raster_bbox <- st_bbox(raster) |>
   st_set_crs(crs(raster)) #needs to have CRS set
 
 sites_buffer <-
-  sites_sf |> 
-  #filter sites to only those within raster boundaries
-  st_intersection(raster_bbox) %>% 
+  sites_sf %>% 
+  # #filter sites to only those within raster boundaries
+  st_intersection(raster_bbox) %>%
   #create circle polygons with buffer in meters
-  st_buffer(dist = set_units(.$buffer_radius_m, "m")) |> 
+  st_buffer(dist = set_units(.$radius, "m")) |> 
   #add column with folder name on snow
   mutate(product = path_file(path_dir(file_path)))
 
 # extract total AGB from each site
-df <- exact_extract(raster, sites_buffer, fun = "sum", append_cols = TRUE, force_df = TRUE)
+df <- exact_extract(
+  raster,
+  sites_buffer,
+  fun = "sum",
+  append_cols = TRUE,
+  force_df = TRUE
+)
 
 # tidy data
-df |> 
+df_tidy <- df |> 
   pivot_longer(
     starts_with("sum."),
     names_to = "year",
     names_prefix = "sum.",
     values_to = "agb"
   )
+
+# plot data to see if it worked
+library(ggplot2)
+ggplot(df_tidy, aes(x = year, y = agb, group = SITE_ID, color = SITE_ID)) +
+  geom_line(alpha = 0.4) +
+  geom_point(aes(size = radius), alpha = 0.4) + #not good viz, just seeing if radius matters much
+  theme(legend.position = "none")
